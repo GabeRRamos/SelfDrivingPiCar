15c6d0d3e067b62fafa4708be0387d70fe0c1ad8
Gabe Ramos <75721734+GabeRRamos@users.noreply.github.com>
2024-12-10 02:01:32 -0800

    Documentation

--- MainRobotLane.py
+++ MainRobotLane.py
@@ -3,28 +3,28 @@ from lanePicker import getLaneCurve
 import WebcamModule
 import cv2
 
-
-##########################################
+# Sets up Motor class
+# Sends signals out to corresponding GPIO pins
 motor = Motor(4,17,27,25,23,24)
-##########################################
+
 
 def main():
 
     img = WebcamModule.getImg()
     curveVal = getLaneCurve(img,1)
 
-    sen = 1.3
-    maxVAL = 0.3
+    sen = 1.5
+    maxVAL = 0.5
     if curveVal>maxVAL:
         curveVal = maxVAL
     if curveVal<-maxVAL:
         curveVal=-maxVAL
     
     if curveVal>0:
-        sen = 1.7
-        if curveVal <0.05: curveVal = 0
+        sen = 1
+        if curveVal <0.1: curveVal = 0
     else:
-        if curveVal>-0.08: curveVal = 0
+        if curveVal>-0.1: curveVal = 0
 
     
     motor.move(0.20,-curveVal*sen,0.05)
